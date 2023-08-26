3ba69a6a9cba15aca72ccd714276e1d158b6bcd1
Konstantinos <zeus_kosta@hotmail.com>
2023-08-26 13:09:23 +0200

    contact data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,29 @@
 import { client } from "../../lib/client";
+import Contact from '../components/Contact'
 
 
 
-export default function Home({ menuData }) {
+export default function Home({ menuData, contactData }) {
   console.log(menuData)
+  console.log(contactData)
   return (
 
     <div>
       <h1 className="text-4xl font-bold text-center">Hello World</h1>
+      <Contact contactData={contactData} />
     </div>
   )
 }
 
 export async function getStaticProps() {
   const menuQuery = '*[_type == "menu"]'
+  const contactQuery = '*[_type == "contact"]'
   const menuData = await client.fetch(menuQuery)
+  const contactData = await client.fetch(contactQuery)
   return {
     props: {
-      menuData
+      menuData,
+      contactData
     }
   }
 }
