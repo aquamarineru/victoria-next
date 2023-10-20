a95b65c54d00cb2983b586fb39b19b0d2d71c7ea
Marina Ilyushina <m_ilyushina@hotmail.com>
2023-10-20 17:31:23 +0200

    gtag.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,18 @@ import Script from 'next/script'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-[tracking-id]" /> {/* Global Site Tag (gtag.js) - Google Analytics здесь нужно добавть id вот так G-1234567890*/}
+        <Script id="google-analytics" strategy="beforeInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+            
+          gtag('config', 'G-[tracking-id]');
+          `} {/* Global Site Tag (gtag.js) - Google Analytics здесь нужно добавть id вот так G-1234567890*/}Н
+        </Script>
+      </Head>
       <body>
         <Main />
         <NextScript />
